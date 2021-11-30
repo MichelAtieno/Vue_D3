e0897fb8529f6abdec47e2ad31bbb58ef1a76eda
Michel Atieno <mishqamish@gmail.com>
2021-11-30 15:13:53 +0300

    Initial setup

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+   
+        <svg width="800" height="500">
+            <circle r="10"
+                    v-for="(item, index) in dataset"
+                    :cx="item[0]"
+                    :cy="item[1]"
+                    :key="index"
+                    fill="#000"
+                    transform="translate(350, 320)" />
+        </svg>
+   
+
+
+</template>
+
+<script>
+    import dataset from './dataset'
+    export default {
+        data() {
+            return {
+                dataset
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
