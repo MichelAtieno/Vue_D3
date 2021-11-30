20f522bf6f6f802486f91f081918ec294368be45
Michel Atieno <mishqamish@gmail.com>
2021-11-30 15:45:16 +0300

    Connected the dots

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,19 @@
 <template>
    
         <svg width="800" height="500">
+            <path stroke="green"
+                  fill="none"
+                  stroke-width="5"
+                  :d="d"
+                  transform="translate(350, 320)"   />
             <circle r="10"
                     v-for="(item, index) in dataset"
                     :cx="item[0]"
                     :cy="item[1]"
                     :key="index"
                     fill="#000"
-                    transform="translate(350, 320)" />
+                    transform="translate(350, 320)"
+                    @click="onClick(item)" />
         </svg>
    
 
@@ -15,11 +21,27 @@
 </template>
 
 <script>
+    import * as d3 from 'd3'
     import dataset from './dataset'
     export default {
         data() {
             return {
                 dataset
+            }
+        },
+        methods: {
+            onClick(item) {
+                console.log('hey you', item)
+            }
+        },
+        computed: {
+            lineGenerator() {
+                return d3.line()
+                         .x(v => v[0])
+                         .y(v => v[1])   
+            },
+            d() {
+                return this.lineGenerator(this.dataset)
             }
         }
     }
